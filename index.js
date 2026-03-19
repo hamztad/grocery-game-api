0b62e9d11a672e3c1a0f31f9f9e841759de50773
hamztad <hamztad@gmail.com>
2026-03-19 10:26:05 +0100

    Initialize Express server with basic route

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
